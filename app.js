fb55c4dbf359b531fb7525c0a6bcaf982da05e42
vkguru <30696225+vkguru@users.noreply.github.com>
2019-11-05 03:49:27 +0100

    Revert "Travis ci"

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use((req, res) => {
-  res.json({ message: 'Your request was successful!' });
+   res.json({ message: 'Your request was successful!' }); 
 });
 
 module.exports = app;
