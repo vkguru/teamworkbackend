f02f5f118ef86d7174b68348f2cad61f21af1cf7
vkguru <vnwoguru@gmail.com>
2019-11-16 09:35:45 +0100

    node-postgres

---
+++ db2.sql
@@ -0,0 +1,46 @@
+
+CREATE TABLE employees (
+    eid BIGSERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    employees_password VARCHAR(200) NOT NULL,
+    job_role VARCHAR(50) NOT NULL,
+    department VARCHAR(50) NOT NULL,
+    date_created DATE,
+    last_login DATE
+);
+
+CREATE TABLE articles (
+    aid BIGSERIAL NOT NULL PRIMARY KEY,
+    title VARCHAR(300) NOT NULL,
+    body VARCHAR,
+    employee_id  INT REFERENCES employees(eid),
+    author VARCHAR UNIQUE author REFERENCES employees(username),
+    date_created TIMESTAMP,
+    share_article_id INT[] DEFAULT ARRAY[]::INT[],
+    share INT DEFAULT 0
+);
+
+
+CREATE TABLE gifs (
+    gid BIGSERIAL NOT NULL PRIMARY KEY,
+    employee_id  INT REFERENCES employees(eid),
+    author VARCHAR UNIQUE author REFERENCES employees(username),
+    date_created TIMESTAMP,
+    share_gif INT[] DEFAULT ARRAY[]::INT[],
+    share INT DEFAULT 0
+);
+
+CREATE TABLE comments (
+    cid BIGSERIAL NOT NULL PRIMARY KEY,
+    comment VARCHAR(300),
+    employee_id  INT REFERENCES employees(eid),
+    author VARCHAR UNIQUE author REFERENCES employees(username),
+    article_id INT REFERENCES articles(aid),
+    gif_id INT REFERENCES gif(gid),
+    date_created TIMESTAMP,
+    share_post INT[] DEFAULT ARRAY[]::INT[],
+    share INT DEFAULT 0
+);
